2cd41babb44c149a74546ffc7d05fa9a639a56e6
JohnMark Fisher <fisherjohnmark@gmail.com>
2019-06-19 08:21:25 -0400

    stripped down excess
    includes polygon geojson

--- js/main.js
+++ js/main.js
@@ -1,2 +1,55 @@
+function initializeMap() {
+    
+    var OSM = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+    
+    var Imagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    });
+    
+    var map = L.map('map', {
+        layers: [OSM]
+    });
+    
+    var baseMaps = {
+        "Open Street Map": OSM,
+        "Imagery": Imagery
+    };
+    
+    L.control.layers(baseMaps).addTo(map);
 
-//$(document).ready(function);
+    var tractStyle = {
+        fillColor: "#C8102E",
+        weight: 1.5,
+        opacity: .8,
+        color: 'white',
+        dashArray: '4',
+        fillOpacity: 0.4
+    }
+    
+    $.getJSON("data/wi_tracts.geojson", function(data){
+        
+        var features = data.features;
+        
+        var tractsLayer = L.geoJSON(features, {
+            style: tractStyle,
+            onEachFeature: tractData
+        }).addTo(map);
+        
+        map.fitBounds(tractsLayer.getBounds());
+        
+    });
+    
+}; // end initializeMap
+
+function tractData(feature,layer){
+    
+    var popupContent = feature.properties.GEOID+"<br>"+feature.properties.STATEFP;
+    
+    layer.bindPopup(popupContent);
+    
+}; // end of tractData
+
+$(document).ready(initializeMap);
